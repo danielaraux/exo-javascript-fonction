b6d4fa7f40deb086b030425622d83ac12ee716c3
daniel <danielaraux@outlook.com>
2025-06-18 10:57:36 +0200

    niveau-1-fini

---
+++ niveau-1-les-fondamenteaux/exercice-2-salutation-personnalisee/assets/js/script.js
@@ -0,0 +1,11 @@
+// On déclare notre fonction avec l'argument nom.
+// On affiche la phrase bonjour avec l'argument nom.
+// On appelle notre fonction saluerNom en précisant pour chaque appel, un prénom.
+
+function saluerNom(nom) {
+    console.log(`Bonjour, ${nom} !`)
+}
+
+saluerNom("Alice")
+saluerNom("Henri")
+saluerNom("Jamie")
---
+++ niveau-1-les-fondamenteaux/exercice-3-calcul-de-carre/assets/js/script.js
@@ -0,0 +1,13 @@
+// On crée notre fonction avec en argument nombre
+//On retourne le carré (nombre x nombre)
+// On crée une variable qui va stocker notre fonction et dans la fonction on met un nombre
+//qui va etre utilisé pour faire le carré
+// enfin, on affiche le résultat en console.log avec notre nouvelle variable
+
+function calculerCarre(nombre) {
+    return nombre * nombre
+}
+
+let result = calculerCarre(9)
+
+console.log(result)
